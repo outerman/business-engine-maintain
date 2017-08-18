6d0d6b93c6a793d1165e932dbcefce25c9de66a6
fsk <fsk@rrtimes.com>
2017-08-18 14:44:52 +0800

    pages

--- mock.js
+++
@@ -1,8 +0,0 @@
-
-import './apps/acm-voucher/mock.js';
-import './apps/login/mock.js';
-import './apps/mk-app-complex-table/mock.js';
-import './apps/portal/apps/mk-app-portal-about/mock.js';
-import './apps/portal/mock.js';
-import './apps/root/apps/mk-app-root-about/mock.js';
-import './apps/root/apps/mk-app-root-helloWorld/mock.js';
